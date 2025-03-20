f8eb90dd721b17185619ea1d5bcabce8010ec49b
TanishkaCh30 <137798637+TanishkaCh30@users.noreply.github.com>
2025-03-20 14:23:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 //complete this code
 class Person {
 	constructor(name,age){
-		this.name = name;
-		this.age = age;
+		this._name = name;
+		this._age = age;
 	}
 	getName(){
-		return this.name;
+		return this._name;
 	}
 	setAge(n){
-		this.age = n;
+		this._age = n;
 	}
 	getAge(){
-		return this.age;
+		return this._age;
 	}
 
 }
 
 class Student extends Person {
 	study(){
-		console.log(`${this.name} is studying`);
+		console.log(`${this._name} is studying`);
 	}
 }
 
 class Teacher extends Person {
 	teach(){
-		console.log(`${this.name} is teaching`);
+		console.log(`${this._name} is teaching`);
 	}
 }
 
